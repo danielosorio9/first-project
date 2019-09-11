bd27516b0c6c91baa7d6f9fc757d146b4d818b4b
Daniel Osorio <daniel.osorio@ucp.edu.co>
2019-09-11 16:27:12 -0500

    Passing data Ep. 4
    Passing data from web.php and PHP vs Blade syntax to display it

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,19 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::view('/', 'welcome');
+Route::view('contact', 'contact');
+Route::view('about', 'about');
+
+Route::get('customers', function(){
+
+    $customers = [
+        'John Doe',
+        'Jane Doe',
+        'Bob the Builder',
+    ];
+
+   return view('internals.customers', [
+       'customers' => $customers,
+   ]);
 });
