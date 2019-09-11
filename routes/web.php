8b262c621939703a6fb7f1420fb978d42eda6797
Daniel Osorio <daniel.osorio@ucp.edu.co>
2019-09-11 16:44:09 -0500

    Create Controller Ep. 5
    Created controller and moved logic from web.php to controller

--- routes/web.php
+++ routes/web.php
@@ -11,19 +11,9 @@
 |
 */
 
+
 Route::view('/', 'welcome');
 Route::view('contact', 'contact');
 Route::view('about', 'about');
 
-Route::get('customers', function(){
-
-    $customers = [
-        'John Doe',
-        'Jane Doe',
-        'Bob the Builder',
-    ];
-
-   return view('internals.customers', [
-       'customers' => $customers,
-   ]);
-});
+Route::get('customers', 'CustomersController@index');
